b063885c7603119e258f31ec8d9e6fa6e604eeac
AmandaWuyy <wyyhawkeye@gmail.com>
2020-03-21 10:39:29 +0800

    (3)查找文件中的数字并以字符串格式输出_入门

---
+++ FindDigits2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# 打开并读取文件里的字符串
+with open('/tmp/String.txt') as f:
+    s = f.read()
+res = ""
+
+# 循环字符串里每个字符，判定是否为数字
+for char in s:
+    if char.isdigit():
+        res += char
+print(res)
